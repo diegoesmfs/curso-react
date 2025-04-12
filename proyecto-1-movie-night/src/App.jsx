a0a966fc9d3690f564e70752d413c89a7443ed79
Diego Morales <Diegoalef1311moralef@gmail.com>
2025-04-12 18:44:41 -0400

    cambios para tener componentes con ejemplos aparte(me da flojera crear otro proyecto)

--- proyecto-1-movie-night/src/App.jsx
+++ proyecto-1-movie-night/src/App.jsx
@@ -1,38 +1,12 @@
 import './App.css'
-import { MiComponente } from './MiComponente'
-import { TercerComponente } from './TercerComponente'
-import { SegundoComponente } from './SegundoComponente'
-import { EventosComponente } from './EventosComponente'
+
 function App() {
-
-  const fichaMedica = {
-
-    altura: "167cm",
-    grupo: "O+",
-    estado: "Bien",
-    alergias: "Polvo"
-
-  }
 
   return (
     <>
       <h1>Vite + React</h1>
 
-      <div className='componentes'>
-
-        <MiComponente />
-        <hr />
-        <SegundoComponente />
-        <hr />
-        <TercerComponente 
-        nombre = "Diego"
-        ficha={fichaMedica}
-        />
-        <hr />
-        <EventosComponente/>
-
-      </div>
-
+  
     </>
   )
 }
