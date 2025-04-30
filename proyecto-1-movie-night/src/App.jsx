cd56980d64b802c7068c5636c7e4e9f1d54fa272
Diego Morales <Diegoalef1311moralef@gmail.com>
2025-04-30 15:49:16 -0400

    otro ejercicio utilizando la referencia useRef

--- proyecto-1-movie-night/src/App.jsx
+++ proyecto-1-movie-night/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
 import { Form } from '../components/Form';
+import { Example } from '../components/Example';
 
 function App() {
 
   return (
     <div>
+
+      <Example />
+
+      <hr />
+
       <Form />
     </div>
   )
